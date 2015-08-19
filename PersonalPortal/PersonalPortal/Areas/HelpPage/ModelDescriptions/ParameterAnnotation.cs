30c161929dddb842d6be16c6538d860ea053170c
mk-soft <kohut.mykola@live.com>
2015-08-19 19:20:21 +0300

    created new project

---
+++ PersonalPortal/PersonalPortal/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PersonalPortal.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
